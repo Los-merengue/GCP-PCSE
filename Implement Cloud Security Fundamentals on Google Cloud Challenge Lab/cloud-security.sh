f0bb8c4d1ad2b6257c8a36906e587dc6eed29b37
Emeka Michael Nzeopara <michaels4uandme@gmail.com>
2024-04-24 01:33:48 +0300

    Added the Task 4 script

--- Implement Cloud Security Fundamentals on Google Cloud Challenge Lab/cloud-security.sh
+++ Implement Cloud Security Fundamentals on Google Cloud Challenge Lab/cloud-security.sh
@@ -35,3 +35,9 @@ gcloud projects add-iam-policy-binding $DEVSHELL_PROJECT_ID --member serviceAcco
 
 gcloud projects add-iam-policy-binding $DEVSHELL_PROJECT_ID --member serviceAccount:$SERVICE_ACCOUNT@$DEVSHELL_PROJECT_ID.iam.gserviceaccount.com \
 --role projects/$DEVSHELL_PROJECT_ID/roles/$CUSTOM_SECURITY_ROLE
+
+## Task 
+
+gcloud container clusters create $CLUSTER_NAME --num-nodes 1 --master-ipv4-cidr=172.16.0.64/28 --network orca-build-vpc --subnetwork orca-build-subnet \
+--enable-master-authorized-networks --master-authorized-networks 192.168.10.2/32 --enable-ip-alias --enable-private-nodes --enable-private-endpoint \ 
+--service-account $SERVICE_ACCOUNT@$DEVSHELL_PROJECT_ID.iam.gserviceaccount.com --zone $ZONE
