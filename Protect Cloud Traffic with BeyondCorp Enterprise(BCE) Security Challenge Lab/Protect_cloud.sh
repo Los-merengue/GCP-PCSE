1cec94e4a926043bde426cfa18a3cba6c07240a8
Emeka Michael Nzeopara <michaels4uandme@gmail.com>
2024-06-03 03:24:32 +0300

    Modify thee script

--- Protect Cloud Traffic with BeyondCorp Enterprise(BCE) Security Challenge Lab/Protect_cloud.sh
+++ Protect Cloud Traffic with BeyondCorp Enterprise(BCE) Security Challenge Lab/Protect_cloud.sh
@@ -1,4 +1,4 @@
-TASK 1:-
+#TASK 1:-
 
 git clone https://github.com/GoogleCloudPlatform/python-docs-samples.git
 cd python-docs-samples/appengine/standard_python3/hello_world/
@@ -6,7 +6,7 @@ gcloud app create --project=$(gcloud config get-value project) --region=us-centr
 gcloud app deploy
 
 
-TASK 2:-
+#TASK 2:-
 
 export AUTH_DOMAIN=$(gcloud config get-value project).uc.r.appspot.com
 echo $AUTH_DOMAIN
